430799eaff4333e56bede78f81802a6925a6d1df
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-06-12 13:41:45 +0530

    Time: 4 ms (40.90%) | Memory: 44.2 MB (19.88%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Stack<Integer> st = new Stack();
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+
+        for(int num: nums2){
+            while(!st.isEmpty() && st.peek() < num){
+                mpp.put(st.pop(), num);
+            }
+            st.push(num);
+        }
+        for(int num: nums1){
+            if(mpp.containsKey(num)){
+                ans.add(mpp.get(num));
+            }else{
+                ans.add(-1);
+            }
+        }
+        
+        int arr[] = new int[ans.size()];
+        for(int i = 0; i<ans.size(); i++){
+            arr[i] = ans.get(i);
+        }
+
+        return arr;
+    }
+}
