49ac9f4ddf52496ef7d76049d3981c7ee4ff06d4
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-06-20 10:34:40 +0530

    Time: 31 ms (63.20%) | Memory: 59.8 MB (33.75%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -5,21 +5,21 @@ class Solution {
         int idx = 0;
         Deque<Integer> q = new ArrayDeque<>();
 
-        for(int i = 0; i<n; i++){
-            if(!q.isEmpty() && q.peek() == i - k){
+        for(int i =0; i<n; i++){
+            if(!q.isEmpty() && q.peek() == i-k){
                 q.poll();
             }
 
-            while (!q.isEmpty() && nums[q.peekLast()] < nums[i]) {
+            while(!q.isEmpty() && nums[q.peekLast()] < nums[i]){
                 q.pollLast();
             }
 
             q.offer(i);
-            if (i >= k - 1) {
+
+            if(i>=k-1){
                 maxes[idx++] = nums[q.peek()];
             }
         }
         return maxes;
-
     }
 }
