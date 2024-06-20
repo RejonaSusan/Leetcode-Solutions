a1ef9425049f744037f6439e720ee847646059d6
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-06-20 15:32:14 +0530

    Time: 37 ms (71.73%) | Memory: 49.5 MB (73.06%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,7 +1,6 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        Stack<Integer> st1 = new Stack();
-        Stack<Integer> st2 = new Stack();
+        Stack<Integer> st = new Stack();
         int n = arr.length;
         int left[] = new int[n];
         int right[] = new int[n];
@@ -9,20 +8,14 @@ class Solution {
         Arrays.fill(left, -1);
         Arrays.fill(right, n);
 
-        for (int i = 0; i < n; i++) {
-            while (!st1.isEmpty() && arr[st1.peek()] > arr[i]) {
-                st1.pop();
+        for (int i = 0; i < arr.length; ++i) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                int index = st.pop();
+                right[index] = i;
             }
-            left[i] = st1.isEmpty() ? -1 : st1.peek();
-            st1.push(i);
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st2.isEmpty() && arr[st2.peek()] >= arr[i]) {
-                st2.pop();
-            }
-            right[i] = st2.isEmpty() ? n : st2.peek();
-            st2.push(i);
+            if (!st.isEmpty())
+                left[i] = st.peek();
+            st.push(i);
         }
 
         long ans = 0;
