ab9fbec50c696cb61d3715da10308fd3759f4a11
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-07-28 17:09:24 +0530

    Time: 95 ms (21.29%) | Memory: 56.4 MB (38.58%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,41 +1,46 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int tot_sum = 0;
-        for(int i =0; i<n; i++){
-            tot_sum += nums[i];
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += nums[i];
         }
-
-        if(tot_sum % 2 != 0){
+        
+        if(sum % 2 != 0){
             return false;
         }
+        int k = sum/2;
 
-        int k = tot_sum/2;
         int dp[][] = new int[n][k+1];
         for(int r[]: dp){
             Arrays.fill(r, -1);
         }
-        return ans(n-1, k, nums, dp);
+
+        return ans(nums, n-1, k, dp);
     }
 
-    public boolean ans(int id, int sum, int nums[], int dp[][]){
+    public boolean ans(int nums[], int id, int sum, int dp[][]){
+        if(id == 0){
+            return nums[0] == sum;
+        }
 
         if(sum == 0){
             return true;
         }
-        if(id == 0){
-            return nums[id] == sum;
-        }
+
         if(dp[id][sum] != -1){
-            return dp[id][sum] == 0 ? false : true;
+            return dp[id][sum] == 1;
         }
 
-        boolean nopick = ans(id-1, sum, nums, dp);
+        boolean nopick = ans(nums, id-1, sum, dp);
         boolean pick = false;
+
         if(nums[id] <= sum){
-            pick = ans(id-1, sum-nums[id], nums, dp);
+            pick = ans(nums, id-1, sum-nums[id], dp);
         }
+
         dp[id][sum] = nopick || pick ? 1 : 0;
-        return pick || nopick;
+        
+        return nopick || pick;
     }
 }
