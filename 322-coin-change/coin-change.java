62de5ab77dfcb0c8423dde5d49f1e0373bea3649
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-08-08 16:11:36 +0530

    Time: 27 ms (30.72%) | Memory: 45 MB (24.14%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,40 +2,29 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         int dp[][] = new int[n][amount+1];
-        
-        for(int r[] : dp){
-            Arrays.fill(r, -1);
-        }
-
-        int res = ans(coins, amount, n-1, dp);
-
-        if(res == (int)Math.pow(10, 9)){
-            return -1;
-        }
-
-        return res;
-    }
-
-    public int ans(int arr[], int T, int id, int dp[][]){
-
-        if(id == 0){
-            if(T%arr[0] == 0){
-                return T/arr[0];
+    
+        for(int T = 0; T<=amount; T++){
+            if(T%coins[0] == 0){
+                dp[0][T] = T/coins[0];
             }else{
-                return (int)Math.pow(10, 9);
+                dp[0][T] = (int)Math.pow(10,9);
             }
         }
 
-        if(dp[id][T] != -1){
-            return dp[id][T];
+        for(int id = 1; id<n ; id++){
+            for(int T = 0; T<=amount; T++){
+                int notake = dp[id-1][T];
+                int take = Integer.MAX_VALUE;
+                if(coins[id]<=T){
+                    take = 1 + dp[id][T-coins[id]];
+                }
+                dp[id][T] = Math.min(take, notake);
+            }
         }
 
-        int notake = ans(arr, T, id-1, dp);
-        int take = Integer.MAX_VALUE;
-        if(arr[id]<=T){
-            take = 1 + ans(arr, T-arr[id], id, dp);
+        if(dp[n-1][amount] == (int)Math.pow(10, 9)){
+            return -1;
         }
-
-        return dp[id][T] = Math.min(take, notake);
+        return dp[n-1][amount];
     }
 }
