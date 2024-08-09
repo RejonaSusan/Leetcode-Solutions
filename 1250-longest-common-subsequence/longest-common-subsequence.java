cf85c4e6ad3bed23a6ce2c878c995c5b5783c270
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-08-09 19:13:43 +0530

    Time: 20 ms (73.79%) | Memory: 51 MB (19.95%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        int n1 = text1.length();
-        int n2 = text2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
 
-        int dp[][] = new int[n1][n2];
+        int dp[][] = new int[n1+1][n2+1];
 
-        for(int r[] : dp){
-            Arrays.fill(r, -1);
+        for(int i =0; i<=n1; i++){
+            dp[i][0] = 0;
         }
 
-        return ans(text1, text2, n1-1, n2-1, dp);
-    }
-
-    public int ans(String s1, String s2, int id1, int id2, int dp[][]){
-
-        if(id1 < 0 || id2 < 0){
-            return 0;
+        for(int i =0; i<=n2; i++){
+            dp[0][i] = 0;
         }
 
-        if(dp[id1][id2] != -1){
-            return dp[id1][id2];
+        for(int id1 = 1; id1<=n1; id1++){
+            for(int id2 = 1; id2<=n2; id2++){
+                if(s1.charAt(id1-1) == s2.charAt(id2-1)){
+                    dp[id1][id2] = 1 + dp[id1-1][id2-1];
+                }else{
+                    dp[id1][id2] = Math.max(dp[id1][id2-1], dp[id1-1][id2]);
+                }
+            }
         }
 
-        if(s1.charAt(id1) == s2.charAt(id2)){
-            return dp[id1][id2] = 1 + ans(s1, s2, id1-1, id2-1, dp);
-        }else{
-            return dp[id1][id2] = Math.max(ans(s1, s2, id1-1, id2, dp), ans(s1, s2, id1, id2-1, dp));
-        }
+        return dp[n1][n2];
     }
 }
