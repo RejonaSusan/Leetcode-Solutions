b08b53af9d0da6cda4c973cec761faeaf0b42203
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-08-12 17:15:24 +0530

    Time: 1 ms (90.01%) | Memory: 45.8 MB (41.02%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,42 +1,38 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    public int maxProfit(int[] arr) {
 
-        int n = prices.length;
-
+        int n = arr.length;
         int dp[][] = new int[n+1][2];
+
+        int curr[] = new int[2];
+        int after[] = new int[2];
 
         for(int r[] : dp){
             Arrays.fill(r, -1);
         }
 
-        // dp[n][0] = dp[0][n] = 0;
-
-        // for(int i = 0; i>)
-
-        return ans(prices, 0, 0, dp);
-        
-    }
-    public int ans(int arr[], int buy, int id, int dp[][]){
-        if(id == arr.length){
-            return 0;
-        }
-
-        if(dp[id][buy] != -1){
-            return dp[id][buy];
-        }
+        dp[n][0] = dp[n][1] = 0;
 
         int op1 = 0, op2 = 0;
 
-        if(buy == 0){
-            op1 = ans(arr, 0, id+1, dp);
-            op2 = -arr[id] + ans(arr, 1, id+1, dp);
+        for(int i = n-1; i>=0; i--){
+            for(int j = 0; j<=1; j++){
+                if(j == 0){
+                    op1 = after[0];
+                    op2 = -arr[i] + after[1];
+                }
+
+                if(j == 1){
+                    op1 = after[1];
+                    op2 = after[0] + arr[i];
+                }
+
+                curr[j] = Math.max(op1, op2);
+            }
+            after = curr;
         }
 
-        if(buy == 1){
-            op1 = ans(arr, 1, id+1, dp);
-            op2 = ans(arr, 0, id+1, dp) + arr[id];
-        }
-
-        return dp[id][buy] = Math.max(op1, op2);
+        return curr[0];
+        
     }
 }
