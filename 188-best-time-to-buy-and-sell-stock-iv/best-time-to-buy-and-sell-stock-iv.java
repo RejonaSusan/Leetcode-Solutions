9481a5039a90e3803e2348499cf2b12ad33e92e8
Rejona Susan. J <141655755+RejonaSusan@users.noreply.github.com>
2024-08-12 17:10:00 +0530

    Time: 1 ms (100.00%) | Memory: 41.2 MB (96.25%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -2,7 +2,10 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
 
-        int dp[][][] = new int[n+1][2][k+1];
+        // int dp[][][] = new int[n+1][2][k+1];
+
+        int curr[][] = new int[2][k+1];
+        int after[][] = new int[2][k+1];
 
         int op1 = 0, op2 = 0;
 
@@ -10,20 +13,21 @@ class Solution {
             for(int j = 0; j<=1; j++){
                 for(int l = 1; l<=k; l++){
                     if(j == 0){
-                        op1 = dp[i+1][0][l];
-                        op2 = dp[i+1][1][l] - prices[i];
+                        op1 = after[0][l];
+                        op2 = after[1][l] - prices[i];
                     }
                     if(j == 1){
-                        op1 = dp[i+1][1][l];
-                        op2 = dp[i+1][0][l-1] + prices[i];
+                        op1 = after[1][l];
+                        op2 = after[0][l-1] + prices[i];
                     }
 
-                    dp[i][j][l] = Math.max(op1, op2);
+                    curr[j][l] = Math.max(op1, op2);
                 }
             }
+            after = curr;
         }
 
-        return dp[0][0][k];
+        return after[0][k];
 
     }
 }
